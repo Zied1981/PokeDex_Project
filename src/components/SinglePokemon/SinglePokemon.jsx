1504eb1defd1481a46f4d4575b89c239a5f9ae0d
Zied1981 <zkhemiri@gmail.com>
2024-03-18 15:46:12 +0100

    2 push

---
+++ src/components/SinglePokemon/SinglePokemon.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+import "./SinglePokemon.css";
+const SinglePokemon = ({ bild /* singleData  */ }) => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetch(`${bild}`)
+      .then((res) => res.json())
+      .then((fetch) => setData(fetch))
+
+      .catch((err) => console.log("fehler im fetch", err));
+  }, []);
+
+  console.log(data);
+  if (data.id < 10) {
+    return "00";
+  }
+
+  return data ? (
+    <section>
+      <p>{data.id}</p>
+      <img src={data.sprites.front_default} alt="bilder" />
+    </section>
+  ) : (
+    "laden"
+  );
+};
+
+export default SinglePokemon;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,20 +2,26 @@ import { useState } from "react";
 import "./Home.css";
 import Filterpage from "../../components/Filterpage/Filterpage";
 import BurgerMenü from "../../components/BurgerMenü/BurgerMenü";
+import Logo from "../../components/Logo/Logo";
+import Pokecard from "../../components/Pokecard/Pokecard";
 
 const Home = () => {
-
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <div className="burger-menu">
-      <BurgerMenü onClick={toggleMenu} />
-      {isMenuOpen && <Filterpage />}
-    </div>
-  )
+    <>
+      <Logo />
+      <Pokecard />
+
+      <div className="burger-menu">
+        <BurgerMenü onClick={toggleMenu} />
+        {isMenuOpen && <Filterpage />}
+      </div>
+    </>
+  );
 };
 
 export default Home;
