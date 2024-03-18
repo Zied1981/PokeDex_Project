e528c7f2ba2b6e906cfa11b2139e8dbe129017af
SonoraDE <tobiastischer@outlook.de>
2024-03-18 11:32:47 +0100

    Filterpage

--- src/components/Filterpage/Filterpage.jsx
+++ src/components/Filterpage/Filterpage.jsx
@@ -1,10 +1,37 @@
+import { useEffect, useState } from "react";
 import "./Filterpage.css";
 const Filterpage = () => {
+
+  const [pokemonTypes, setPokemonTypes] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://pokeapi.co/api/v2/type');
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setPokemonTypes(data.results);
+      } catch (error) {
+        console.error('Fetch error:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <section className="filterpage-bg">
-      <h1>Filterseite geöffnet</h1>
-    </section>
-  )
-};
+    <div>
+      <img src="./src/assets/TYPE.png" alt="Type Image" />
+      <ul>
+        {pokemonTypes.map((type, index) => (
+          <li key={index}>{type.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
+}
 
 export default Filterpage;
