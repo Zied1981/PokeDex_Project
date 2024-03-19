d85b5f9af859150d185364b3805dbfdd2f7f9b2c
Frederick Pawelzik <frederickpawelzik@gmail.com>
2024-03-19 09:12:34 +0100

    suchfunktion

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,14 +1,25 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import BurgerMenü from "../BurgerMenü/BurgerMenü";
 import DarkLightmode from "../DarkLightmode/Darklightmode";
 import "./Searchbar.css";
+import { SearchValueContext } from "../../context/context";
+import { useNavigate } from "react-router-dom";
 const SearchBar = () => {
-  const [searchInput, setSearchInput] = useState("");
+  const { searchInput, setSearchInput } = useContext(SearchValueContext);
+
+  let navigate = useNavigate();
+  const goToSite = () => {
+    if (event.key === "Enter") {
+      navigate(`/detail/${searchInput}`);
+      // console.log("neuer link");
+    }
+  };
 
   return (
     <nav>
       <BurgerMenü />
       <input
+        onKeyPress={goToSite}
         onChange={(event) => setSearchInput(event.target.value)}
         value={searchInput}
         type="search"
